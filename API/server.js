9f84a42733dce2f203f871c9f4919e7cdff02de0
Your Name <you@example.com>
2019-09-23 15:18:46 -0700

    having trouble with sqlite3 install

--- API/server.js
+++ API/server.js
@@ -8,6 +8,16 @@ server.get('/test', (req,res) => {
     res.json('it working!')
 })
 
+//GET users
+server.get('/api/users', (req,res) => {
+    db.getUsers()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({message: "Fail to retrieve users"})
+        })
+})
 
 
 
